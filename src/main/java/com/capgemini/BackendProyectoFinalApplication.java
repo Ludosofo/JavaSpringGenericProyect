279818e0bcfd7cc47b7f606a560e3c35067a54d5
ludosofo <jose-ramon.campos-munoz@capgemini.com>
2022-01-18 11:03:10 +0100

    commit inicial

---
+++ src/main/java/com/capgemini/BackendProyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProyectoFinalApplication.class, args);
+	}
+
+}
