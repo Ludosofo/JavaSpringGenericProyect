aa99af63ff8cd535e1d47b3ec78801ef05ec2f1f
ludosofo <jose-ramon.campos-munoz@capgemini.com>
2022-01-24 17:41:11 +0100

    Corrijo el id float y lo pongo long

--- src/main/java/com/capgemini/entities/Usuario.java
+++ src/main/java/com/capgemini/entities/Usuario.java
@@ -1,7 +1,16 @@
 package com.capgemini.entities;
 
 public class Usuario {
-	private float id;
+	private long id;
+	private String alias;
+	private String nombre;
+	private String apellidos;
+	private String pass; // TODO: Investigar sobre seguridad
+	private String avatar; // Esto sería un dato obtenido de una imagen
+	private String mail;
+	private String telefono;
+	private String geo;
+	
 	// alias, nombre, apellidos, pass, avatar, mail, tlfn, geo 
 	
 }
