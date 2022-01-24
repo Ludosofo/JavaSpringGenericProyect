886d794b84b475b6fd81a310a3312f62b12d453c
ludosofo <jose-ramon.campos-munoz@capgemini.com>
2022-01-24 18:48:17 +0100

    Añadimos un comprobador del campo mail

--- src/main/java/com/capgemini/entities/Usuario.java
+++ src/main/java/com/capgemini/entities/Usuario.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -50,7 +51,9 @@ public class Usuario implements Serializable{
 	@Size( min = 8, message = "El password tiene que tener minimo 8 caracteres")
 	private String pass; // TODO: Investigar sobre seguridad
 	
+	// Añadimos un regex para la comprobación del email
 	@NotNull
+	@Email(message = "Correo no es valido", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
 	private String mail;
 
 	// Estos atributos pueden ser null
