61eb7c0973c1debd565d0dd1e6c7bfe4199cfd38
ludosofo <jose-ramon.campos-munoz@capgemini.com>
2022-01-26 18:22:50 +0100

    Base de datos ya creada y relacioada correctamente

--- src/main/java/com/capgemini/entities/OfertaImagenes.java
+++ src/main/java/com/capgemini/entities/OfertaImagenes.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -16,8 +19,8 @@ import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor
 @AllArgsConstructor
-@Table(name="usuario_imagenes")
-public class UsuarioImagen {
+@Table(name="oferta_imagenes")
+public class OfertaImagenes {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,6 +28,7 @@ public class UsuarioImagen {
 	private String relativeURL;
 	private LocalDate fecha_subida;
 	
+	@ManyToOne
+	public Oferta oferta;
+
 }
-
-
