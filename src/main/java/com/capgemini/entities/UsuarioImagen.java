5ef8eefd924d101b529fb4dd9edcc83c08f2f5b2
ludosofo <jose-ramon.campos-munoz@capgemini.com>
2022-01-27 17:09:15 +0100

    # WARNING: head commit changed in the meantime
    Subimos los commit

--- src/main/java/com/capgemini/entities/UsuarioImagen.java
+++ src/main/java/com/capgemini/entities/UsuarioImagen.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -23,7 +24,8 @@ public class UsuarioImagen {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
+	@ManyToOne
 	private Long id_usuario;
 	private String relativeURL;
 	private LocalDate fecha_subida;
