176c8a82d89f6d02b2dde0ee838e7560e0ad7567
Felix <fvicenta@capgemini.com>
2022-02-01 20:49:11 +0100

    Commit aplication properties

--- src/main/java/com/capgemini/servicies/impl/OfertaServImpl.java
+++ src/main/java/com/capgemini/servicies/impl/OfertaServImpl.java
@@ -1,14 +1,6 @@
 package com.capgemini.servicies.impl;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 import org.springframework.stereotype.Service;
 
 import com.capgemini.entities.Oferta;
