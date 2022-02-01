ecb2a740215a239b7929acc4e54dbd846ef7eb2f
ludosofo <jose-ramon.campos-munoz@capgemini.com>
2022-02-01 20:44:43 +0100

    Subo IContratoServices

---
+++ src/main/java/com/capgemini/servicies/IContratoServices.java
@@ -0,0 +1,12 @@
+package com.capgemini.servicies;
+
+import java.util.List;
+
+import com.capgemini.entities.Contrato;
+
+public interface IContratoServices {
+    public List<Contrato> findAll();
+	public Contrato findById(long id);
+    public void delete(long id);
+    public void save(Contrato contrato);
+}
