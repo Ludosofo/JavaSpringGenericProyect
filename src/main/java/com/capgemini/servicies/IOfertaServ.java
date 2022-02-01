cd0d5db448332d89c48e9d402772b2acffa611ae
ludosofo <jose-ramon.campos-munoz@capgemini.com>
2022-02-01 19:54:06 +0100

    Añado calls to jpa

--- src/main/java/com/capgemini/servicies/IOfertaServ.java
+++ src/main/java/com/capgemini/servicies/IOfertaServ.java
@@ -1,9 +1,14 @@
 package com.capgemini.servicies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import com.capgemini.entities.Oferta;
 
 public interface IOfertaServ {
-
+    public List<Oferta> findAll();
+	public Oferta findById(long id);
+    public void delete(long id);
+    public void save(Oferta oferta);
 }
--- src/main/java/com/capgemini/servicies/IUsuarioServicies.java
+++ src/main/java/com/capgemini/servicies/IUsuarioServicies.java
@@ -3,8 +3,13 @@ package com.capgemini.servicies;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import com.capgemini.entities.Usuario;
 
-@Repository
 public interface IUsuarioServicies {
+    public List<Usuario> findAll();
+	public Usuario findById(long id);
+    public void delete(long id);
+    public void save(Usuario oferta);
 }
