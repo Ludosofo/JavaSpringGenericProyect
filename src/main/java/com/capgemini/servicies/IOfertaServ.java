ffc413f5e9f1b466893c3c0e10acdd6aae67d52a
Felix <fvicenta@capgemini.com>
2022-02-02 17:39:40 +0100

    Cambios en implementacion usuarios

--- src/main/java/com/capgemini/servicies/IOfertaServ.java
+++ src/main/java/com/capgemini/servicies/IOfertaServ.java
@@ -1,7 +1,6 @@
 package com.capgemini.servicies;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.List;
 import com.capgemini.entities.Oferta;
--- src/main/java/com/capgemini/servicies/impl/UsuarioServiciesImpl.java
+++ src/main/java/com/capgemini/servicies/impl/UsuarioServiciesImpl.java
@@ -1,28 +1,22 @@
 package com.capgemini.servicies.impl;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 import org.springframework.stereotype.Service;
 
+import com.capgemini.dao.IUsuarioDao;
 import com.capgemini.entities.Usuario;
 import com.capgemini.servicies.IUsuarioServicies;
+
 
 
 // TODO
 @Service
 public class UsuarioServiciesImpl implements IUsuarioServicies{
-
+	private IUsuarioDao daoUsuario;
 	@Override
 	public List<Usuario> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return daoUsuario.findAll();
 	}
 
 	@Override
