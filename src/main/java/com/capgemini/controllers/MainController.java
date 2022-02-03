039c5e7eb51e77d0f3a4ab30c48b5bdbff1f6965
ludosofo <jose-ramon.campos-munoz@capgemini.com>
2022-02-03 19:45:30 +0100

    Ideas de maincontroller

--- src/main/java/com/capgemini/controllers/MainController.java
+++ src/main/java/com/capgemini/controllers/MainController.java
@@ -8,5 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 	
 	//añadir métodos, con sus respectivas conexiones a la capa de servicios
-	
+	// Implementar navegación a diferentes vistas/obtención de datos/procesamiento/etc...
+
+
+	/*
+		- index
+		- getUsuario 		( obtener perfil )
+		- saveUsuario 		( creación/edición )
+		- getListOfertas  	( Hacer un filtro entre las ofertas )
+		- saveUsuarioImagen ( Podría ir aparte y ser llamado por saveUsuario )
+		- saveOferta 		( creación/edición )
+		- saveContrato 		( creación/edición )
+		- deleteOferta  	( )
+		- deleteUsuario 	( darse de baja ? )
+		- getContrato
+		- deleteContrato 	( borrar oferta SOLO si no está cerrado )
+		- saveValoracion 	( creación/valoracion )
+
+		- 
+	*/
 }
