35b0dced9f41d78d6aba7dfd8d875cd2c77e66ba
ludosofo <jose-ramon.campos-munoz@capgemini.com>
2022-02-05 20:26:30 +0100

    Requisitos modificados y html adaptado

--- src/main/java/com/capgemini/entities/Usuario.java
+++ src/main/java/com/capgemini/entities/Usuario.java
@@ -49,13 +49,13 @@ public class Usuario implements Serializable{
 	*/
 
 	@NotNull
-	@NotEmpty(message = "ERROR su password no puede estar vacio")
-	@Size( min = 8, message = "El password tiene que tener minimo 8 caracteres")
+	@NotEmpty(message = "ERROR: su password no puede estar vacio")
+	@Size( min = 8, message = "ERROR: El password tiene que tener minimo 6 caracteres")
 	private String pass; // TODO: Investigar sobre seguridad
 	
 	// Añadimos un regex para la comprobación del email
 	@NotNull
-	@Email(message = "Correo no es valido", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+	@Email(message = "ERROR: Correo enviado no es valido", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
 	private String mail;
 
 	// Estos atributos pueden ser null
