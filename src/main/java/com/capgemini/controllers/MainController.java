c6ec8db28267e8e097d6bd4dac989cbba41c618f
ludosofo <jose-ramon.campos-munoz@capgemini.com>
2022-02-08 16:46:38 +0100

    Mejorado el sistema de login, intentando crear sessiones

--- src/main/java/com/capgemini/controllers/MainController.java
+++ src/main/java/com/capgemini/controllers/MainController.java
@@ -10,6 +10,7 @@ import java.util.Iterator;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.capgemini.entities.Usuario;
 import com.capgemini.servicies.IOfertaServ;
@@ -59,6 +60,7 @@ public class MainController implements Serializable {
 
 	// Cambio para testear
 	// Estás conectado ? Pues debería redireccionarnos a otra pagina
+
 	@GetMapping()
 	public ModelAndView getIndex() {
 		System.out.println("getIndex()");
@@ -66,11 +68,11 @@ public class MainController implements Serializable {
 
 		// Usuario precargado
 		Usuario usuarioDefault = new Usuario();
-		usuarioDefault.setAlias("Nick");
-		usuarioDefault.setNombre("Nombre");
-		usuarioDefault.setPass("password");
-		usuarioDefault.setMail("correo@gmail.com");
-		usuarioDefault.setApellidos("Apellidos");
+		// usuarioDefault.setAlias("Nick");
+		// usuarioDefault.setNombre("Nombre");
+		// usuarioDefault.setPass("password");
+		// usuarioDefault.setMail("correo@gmail.com");
+		// usuarioDefault.setApellidos("Apellidos");
 
 		mav.addObject("usuario", usuarioDefault); // <---- Necesario para que Thymeleaf sepa los datos que recoge
 		mav.addObject("listaUsuarios", usuarioService.findAllByOrderByIdAsc());
@@ -80,64 +82,87 @@ public class MainController implements Serializable {
 
 	// Procesamiento del registro
 	@PostMapping("/register")
-	public ModelAndView saveUsuario(HttpServletResponse response, @ModelAttribute(name = "usuario") Usuario usuario, Model model) {
+	public ModelAndView saveUsuario(HttpServletResponse response, @ModelAttribute(name = "usuario") Usuario usuario,
+			Model model) {
 		ModelAndView mav = new ModelAndView("basic-msg");
 		mav.addObject("redirect", "http://localhost:8080");
 
 		try {
 			usuarioService.save(usuario);
 			mav.addObject("mensaje", "Usuario registrado correctamente");
-			mav.addObject("miliseconds", "2000");	
+			mav.addObject("miliseconds", "2000");
 		} catch (Exception e) {
-			mav.addObject("mensaje", e.getMessage() );
+			mav.addObject("mensaje", e.getMessage());
 			mav.addObject("miliseconds", "9000");
 		}
 
-		// Alguien intento que se pudiera escribir más de 1 caracter conURLEncoder pero no funciona
+		// Alguien intento que se pudiera escribir más de 1 caracter conURLEncoder pero
+		// no funciona
 		// https://stackoverflow.com/questions/38687210/error-with-cookie-value-when-adding-a-new-spring-session/46702343#46702343
-		// response.addCookie( new Cookie("Test", URLEncoder.encode( "Soy una cookie", "UTF-8" ) ));
+		// response.addCookie( new Cookie("Test", URLEncoder.encode( "Soy una cookie",
+		// "UTF-8" ) ));
 		return mav;
 	}
 
 	// Confirmamos el usuario y la contraseña ( ya la cifraremos )
 	// Confirm user and password
-	@PostMapping("checkUsuario")
-	public ModelAndView checkUsuario(
-		HttpServletResponse response,
-		@ModelAttribute(name = "usuario") Usuario request,
-		Model model
-	){
-		
-		// Hacemos una petición al service con la configuración de llamada
-		Usuario usuario = usuarioService.findUsuarioByAliasAndPass(request.getAlias(), request.getPass());
-		if(usuario!=null){
-			usuario.toString();
-		}else{
-			System.out.println("No tenemos usuario");
-			ModelAndView mav = new ModelAndView("index"); // Te echamos a la pagina de entrada
-			return mav;
-		}
-		
-		// Creamos la cookies
-		response.addCookie( new Cookie("getAlias", usuario.getAlias()));
-		response.addCookie( new Cookie("getPass",usuario.getPass()));
-		ModelAndView mav = new ModelAndView("home");
+	// @PostMapping("checkUsuario")
+	// public ModelAndView checkUsuario(
+	// HttpServletResponse response,
+	// @ModelAttribute(name = "usuario") Usuario request,
+	// Model model
+	// ){
 
+	// // Hacemos una petición al service con la configuración de llamada
+	// Usuario usuario =
+	// usuarioService.findUsuarioByAliasAndPass(request.getAlias(),
+	// request.getPass());
+	// if(usuario!=null){
+	// usuario.toString();
+	// }else{
+	// System.out.println("No tenemos usuario");
+	// ModelAndView mav = new ModelAndView("index"); // Te echamos a la pagina de
+	// entrada
+	// return mav;
+	// }
+
+	// // Creamos la cookies
+
+	// ModelAndView mav = new ModelAndView("home");
+
+	// return mav;
+	// }
+
+	@PostMapping("/checkUsuario")
+	public ModelAndView verifyCredentials(HttpServletResponse response, HttpServletRequest request,
+			@ModelAttribute(name = "usuario") Usuario usuario) {
+		Usuario cuenta = usuarioService.findUsuarioByAliasAndPass(usuario.getAlias(), usuario.getPass());
+		ModelAndView mav = new ModelAndView();
+
+		HttpSession session = request.getSession();
+		
+		if (cuenta != null) {
+			// response.addCookie(new Cookie("user", usuario.getAlias()));
+			// response.addCookie(new Cookie("publicKey", "ASSSDSSDFSDFSFSDFSDFSDFSDFSSGGFDGDFSGDF"));
+			session.setAttribute("JSESSIONID", "Session creada");
+			mav.addObject("user", cuenta.toString() );
+			mav.setViewName("welcome");
+		} else {
+
+			// Reset cookies
+			response.addCookie(new Cookie("user", null));
+			response.addCookie(new Cookie("publicKey", null));
+
+			// Enviamos a la redirección
+			mav.addObject("redirect", "http://localhost:8080");
+			mav.addObject("mensaje", "Usuario erroneo");
+			mav.addObject("miliseconds", "2000");
+			mav.setViewName("basic-msg");
+		}
+
+		// mav.addObject( "datos-session", request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);)
 		return mav;
 	}
-
-	@GetMapping("/login")
-	public String verifyCredentials() {
-
-		usuarioService.findUsuarioByAliasAndPass(alias, pass)
-		return "redirect:/home";
-	}
-	
-	
-	// @PostMapping("/formularioLogin")
-	// public String formularioLogin(@ModelAttribute(name = "usuario") Usuario usuario) {		
-	// 	return "redirect:/login";
-	// }
 
 	@GetMapping("/getImgByUser/{id}")
 	public String getImgByUser(@PathVariable(name = "id") Long id, Model model) {
@@ -148,9 +173,9 @@ public class MainController implements Serializable {
 		}
 		return userImgURL;
 	}
-	
+
 	@GetMapping("saveAvatar")
-	public void saveImg(@RequestParam (name="file") MultipartFile avatar){
+	public void saveImg(@RequestParam(name = "file") MultipartFile avatar) {
 	}
 
 	// Limpieza de cookies
@@ -165,28 +190,28 @@ public class MainController implements Serializable {
 			}
 	}
 
-
 }
 
-
 /*
-// SUBIDA DE IMAGEN
-	@PostMapping("/formularioRegistro")
-	public String formularioRegistro() {
-		if (!avatar.isEmpty()) {
-			// Ruta absoluta
-			try {
-				byte[] bytesImages = avatar.getBytes();
-				// Ruta completa, que incluye el nombre original de la imagen
-				Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + avatar.getOriginalFilename());
-				LOG.info("ruta completa la imagen" + rutaCompleta);
-				Files.write(rutaCompleta, bytesImages);
-				usuario.setAvatar(avatar.getOriginalFilename());
-				usuarioService.guardaUsuario(usuario);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return "redirect:/lemonApp";
-	}
-	*/
+ * // SUBIDA DE IMAGEN
+ * 
+ * @PostMapping("/formularioRegistro")
+ * public String formularioRegistro() {
+ * if (!avatar.isEmpty()) {
+ * // Ruta absoluta
+ * try {
+ * byte[] bytesImages = avatar.getBytes();
+ * // Ruta completa, que incluye el nombre original de la imagen
+ * Path rutaCompleta = Paths.get(rutaAbsoluta + "//" +
+ * avatar.getOriginalFilename());
+ * LOG.info("ruta completa la imagen" + rutaCompleta);
+ * Files.write(rutaCompleta, bytesImages);
+ * usuario.setAvatar(avatar.getOriginalFilename());
+ * usuarioService.guardaUsuario(usuario);
+ * } catch (IOException e) {
+ * e.printStackTrace();
+ * }
+ * }
+ * return "redirect:/lemonApp";
+ * }
+ */
